664dcc49983a201cf75956de0f915af251285bc3
Lewis Daly <lewisdaly@me.com>
2019-08-27 14:02:09 +0800

    improve format-docker-images comments

--- .circleci/format-docker-images.sh
+++ .circleci/format-docker-images.sh
@@ -10,7 +10,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # 2. Remove leading and trailing whitespace
 # 3. Collapse into one line, ensuring the pattern: `repo*\ntag:`
 # 4. Remove the `repository:` string, Replace the ` tag: ` string with `:`
-# 5-8. Remove repos that don't work with the license scanner: `mojaloop/documentation, mojaloop/central-kms `
+# 5-8. Remove repos that don't work with the license-scanner
 # 9. Remove rows that are commented out in yaml (start with #)
 # 10. Sort and remove duplicates
 # 11. Collapse list into one line, separated with spaces
