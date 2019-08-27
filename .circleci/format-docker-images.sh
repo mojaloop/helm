fededb4994c02150e88dcd33d61c4f24ebd63c1a
Lewis Daly <lewisdaly@me.com>
2019-08-27 13:50:33 +0800

    working on blacklist

--- .circleci/format-docker-images.sh
+++ .circleci/format-docker-images.sh
@@ -10,18 +10,21 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # 2. Remove leading and trailing whitespace
 # 3. Collapse into one line, ensuring the pattern: `repo*\ntag:`
 # 4. Remove the `repository:` string, Replace the ` tag: ` string with `:`
-# 5. Remove repos that don't work with the license scanner: `mojaloop/documentation, mojaloop/central-kms `
-# 6. Remove rows that are commented out in yaml (start with #)
-# 7. Sort and remove duplicates
-# 8. Collapse list into one line, separated with spaces
-# 9. Remove any leading whitespace
+# 5-8. Remove repos that don't work with the license scanner: `mojaloop/documentation, mojaloop/central-kms `
+# 9. Remove rows that are commented out in yaml (start with #)
+# 10. Sort and remove duplicates
+# 11. Collapse list into one line, separated with spaces
+# 12. Remove any leading whitespace
 ##
 cat ${DIR}/../**/values.yaml \
   | grep 'repository:.mojaloop\|tag:' \
   | awk '{$1=$1;print}' \
   | awk 'BEGIN{FS="   *"} f ~ "repo" && $1 ~ "tag" {print f" "$1} {f=$1}' \
   | sed -e "s/repository://g"  -e "s/ tag: /:/g" \
-  | grep -v "mojaloop/documentation|mojaloop/central-kms|mojaloop/interop-switch|mojaloop/forensic-logging-sidecar" \
+  | grep -v "mojaloop/documentation" \
+  | grep -v "mojaloop/central-kms" \
+  | grep -v "mojaloop/interop-switch" \
+  | grep -v "mojaloop/forensic-logging-sidecar" \
   | grep -v "^#" \
   | sort -u \
   | tr -d '\n' \
