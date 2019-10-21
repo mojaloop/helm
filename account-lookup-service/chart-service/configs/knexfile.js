49f55f54f9540dc1fcdc74dc3d0e4e03812075b5
Vassilis Barzokas <vassilis.barzokas@modusbox.com>
2019-10-21 22:51:53 +0300

    Updated mappings for `configs/default.json` and `configs/knexfile.js`

--- account-lookup-service/chart-service/configs/knexfile.js
+++ account-lookup-service/chart-service/configs/knexfile.js
@@ -1,11 +1,15 @@
 'use strict'
+
 const migrationsDirectory = '/opt/account-lookup-service/migrations'
 const seedsDirectory = '/opt/account-lookup-service/seeds'
+
 const Config = require('/opt/account-lookup-service/src/lib/config')
+
 module.exports = {
     client: 'mysql',
     version: '5.7',
     connection: Config.DATABASE_URI,
+    pool: Config.DATABASE.pool,
     migrations: {
         directory: migrationsDirectory,
         tableName: 'migration',
