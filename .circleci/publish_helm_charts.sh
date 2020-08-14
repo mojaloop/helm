a9b6e7c8e5db6ac4bcfd542872ee9f584c90b8fe
Lewis Daly <lewisdaly@me.com>
2020-08-14 10:01:19 +0800

    fix(publish_helm_chart): commit message quotes

--- .circleci/publish_helm_charts.sh
+++ .circleci/publish_helm_charts.sh
@@ -42,7 +42,7 @@ echo "Staging packaged Helm charts..." | tee git.log
 git add -f repo/*.*
 
 echo "Commiting changes..." | tee git.log 
-git commit -a -m $COMMIT_MESSAGE
+git commit -a -m "'$COMMIT_MESSAGE'"
 
 echo "Publishing $REVISION release to $GITHUB_TARGET_BRANCH on github..." | tee git.log 
 git push -q $GITHUB_PROJECT_USERNAME $GITHUB_TARGET_BRANCH &> git.log
