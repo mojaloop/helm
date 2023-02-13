9fabfd194f36c66f53595c823f03928831ccc525
Kevin Leyow <kleyow@gmail.com>
2023-02-13 13:06:20 -0500

    chore(mojaloop/#3073): edit script to include files and remove -f flag (#545)
    * chore(mojaloop/#3073): edit script to include files and remove unneeded -f
    
    * chore: add files

--- .circleci/publish_helm_charts.sh
+++ .circleci/publish_helm_charts.sh
@@ -22,6 +22,7 @@ git clone -b $GITHUB_TARGET_BRANCH --single-branch $CIRCLE_REPOSITORY_URL $WORKI
 
 echo "Moving packaged charts to release directory and repo folder" | tee git.log
 mv repo/*.* $WORKING_RELEASE_DIRECTORY/repo
+mv README.md LICENSE.md CODEOWNERS $WORKING_RELEASE_DIRECTORY
 
 echo "Switching to release directory" | tee git.log
 cd $WORKING_RELEASE_DIRECTORY
@@ -33,7 +34,7 @@ helm repo index . --url $LOCAL_HELM_MOJALOOP_REPO_URI
 git status
 
 echo "Staging packaged Helm charts..." | tee git.log
-git add -f ./*.*
+git add ./*.* ../README.md ../LICENSE.md ../CODEOWNERS
 
 echo "Commiting changes..." | tee git.log
 git commit -a -m "'$COMMIT_MESSAGE'"
