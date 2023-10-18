93a7244f54e3134f1297cb34f977366d15cfd99e
Vijay <vijaya.guthi@infitx.com>
2023-10-18 17:12:22 +0530

    feat: added batch processing for position prepare messages

---
+++ centralledger/chart-handler-transfer-position-batch/configs/knexfile.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const migrationsDirectory = '/opt/app/migrations'
+const seedsDirectory = '/opt/app/seeds'
+
+const Config = require('/opt/app/src/lib/config')
+
+module.exports = {
+    client: 'mysql',
+    connection: Config.DATABASE.connection,
+    pool: Config.DATABASE.pool,
+    migrations: {
+        directory: migrationsDirectory,
+        tableName: 'migration',
+        stub: `${migrationsDirectory}/migration.template`
+    },
+    seeds: {
+        directory: seedsDirectory,
+        loadExtensions: ['.js']
+    }
+}
