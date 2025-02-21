bf9ce9e09ad79145d2356977a4077be458a315d1
Kalin Krustev <kalin.krustev@gmail.com>
2025-02-21 08:59:23 +0000

    feat: add linting configuration and scripts for Helm charts

---
+++ lint-render.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+#
+# Script to Lint all charts
+#
+
+echo "Linting all Charts..."
+
+set -e
+
+LOCAL_HELM_MOJALOOP_REPO_URI=${HELM_MOJALOOP_REPO_URI:-'https://docs.mojaloop.io/helm/repo'}
+
+trap 'echo "Command failed...exiting. Please fix me!"' ERR
+
+rm -rf test
+mkdir test
+
+if [ "$1" ]; then
+    declare -a charts=("$1")
+else
+    declare -a charts=(
+        example-mojaloop-backend
+        ml-testing-toolkit
+        ml-testing-toolkit-cli
+        sdk-scheme-adapter/chart-service
+        sdk-scheme-adapter
+        mojaloop-ttk-simulators/chart-sim1
+        mojaloop-ttk-simulators/chart-sim2
+        mojaloop-ttk-simulators/chart-sim3
+        mojaloop-ttk-simulators
+        eventstreamprocessor
+        simulator
+        monitoring/promfana
+        monitoring/efk
+        account-lookup-service
+        als-oracle-pathfinder
+        als-msisdn-oracle
+        # centralkms # Deprecated - No longer supported
+        # forensicloggingsidecar # Deprecated - No longer supported
+        centralledger
+        # centralenduserregistry # Deprecated - No longer supported
+        centralsettlement
+        emailnotifier
+        centraleventprocessor
+        ml-api-adapter
+        quoting-service
+        # finance-portal # Deprecated - No longer supported
+        # finance-portal-settlement-management # Deprecated - No longer supported
+        transaction-requests-service
+        bulk-centralledger/
+        bulk-api-adapter/
+        mojaloop-bulk/
+        mojaloop-simulator
+        mojaloop
+        # kube-system/ntpd/ # Deprecated - No longer supported
+        ml-operator
+        thirdparty/chart-auth-svc
+        thirdparty/chart-consent-oracle
+        thirdparty/chart-tp-api-svc
+        inter-scheme-proxy-adapter
+    )
+fi
+
+echo "\n"
+
+for chart in "${charts[@]}"
+do
+    echo "---=== Rendering $chart ===---"
+    helm template $chart | yq -s '"test/'$chart'/" + .kind + "-" + .metadata.name'
+done
+
