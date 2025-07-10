e75f42119a2f8382e5850e5f85c65521cc4a7224
Yevhen Kyriukha <kirgene@gmail.com>
2025-07-10 20:21:53 +0300

    fix(knex): switch to mysql2 client

--- account-lookup-service/chart-service/configs/knexfile.js
+++ account-lookup-service/chart-service/configs/knexfile.js
@@ -3,7 +3,7 @@ const migrationsDirectory = '/opt/app/migrations'
 const seedsDirectory = '/opt/app/seeds'
 const Config = require('/opt/app/src/lib/config')
 module.exports = {
-    client: 'mysql',
+    client: 'mysql2',
     version: '5.7',
     connection: Config.DATABASE.connection,
     pool: Config.DATABASE.pool,
