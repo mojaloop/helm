7a54b3dda63bd0df33785de2e53b2f11a309eabd
Yevhen Kyriukha <kirgene@gmail.com>
2025-07-10 20:30:26 +0300

    chore: cleanup

--- account-lookup-service/chart-service/configs/knexfile.js
+++ account-lookup-service/chart-service/configs/knexfile.js
@@ -4,7 +4,6 @@ const seedsDirectory = '/opt/app/seeds'
 const Config = require('/opt/app/src/lib/config')
 module.exports = {
     client: 'mysql2',
-    version: '5.7',
     connection: Config.DATABASE.connection,
     pool: Config.DATABASE.pool,
     migrations: {
