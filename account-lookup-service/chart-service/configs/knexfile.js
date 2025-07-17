6861ab34b0154dd663fe5f7f7b07b4d80b1587ee
Vijay <vijaya.guthi@infitx.com>
2025-07-17 15:28:22 +0530

    fix: issues

--- account-lookup-service/chart-service/configs/knexfile.js
+++ account-lookup-service/chart-service/configs/knexfile.js
@@ -3,10 +3,7 @@ const migrationsDirectory = '/opt/app/migrations'
 const seedsDirectory = '/opt/app/seeds'
 const Config = require('/opt/app/src/lib/config')
 module.exports = {
-    client: 'mysql',
-    version: '5.7',
-    connection: Config.DATABASE.connection,
-    pool: Config.DATABASE.pool,
+    ...Config.DATABASE,
     migrations: {
         directory: migrationsDirectory,
         tableName: 'migration',
